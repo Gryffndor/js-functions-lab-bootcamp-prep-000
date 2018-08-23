da075eba09f120b3a7472a26b595012037e20743
Griffin H Stein <grste001@mail.goucher.edu>
2018-08-23 18:35:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ function happyHolidays() {
   return "Happy holidays!"
 }
 
+function happyHolidaysTo(name) {
+  return "Happy Holidays, ${name} !"
+}
+
+function holidayCountdown(holiday, days) {
+  return "It's ${days} days untill ${holiday}!" }
+  
+
+  
+
+
 
 
 
